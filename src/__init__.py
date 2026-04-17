c137b25ca8d0f6545b724a940875e5d09e687e04
bl1nkless <bl1nkless@users.noreply.github.com>
2026-04-17 11:07:20 +0300

    feat: add file writing function

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@
 
 from src.file_reader import filter_lines_by_keyword
 from src.file_reader import read_txt_file
+from src.file_reader import write_filtered_file
--- src/file_reader.py
+++ src/file_reader.py
@@ -12,3 +12,7 @@ def read_txt_file(file_path: str) -> list[str]:
 
 def filter_lines_by_keyword(lines: list[str], keyword: str) -> list[str]:
     return [line for line in lines if keyword in line]
+
+
+def write_filtered_file(lines: list[str], output_path: str = "filtered.txt") -> None:
+    Path(output_path).write_text("\n".join(lines), encoding="utf-8")
